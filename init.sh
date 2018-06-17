249a361a70bdd34c700d70753695ae78a8cfb774
Dimitri Wahyudiputra <gitmitri@gmail.com>
2018-06-18 04:28:55 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -11,7 +11,7 @@ sudo true;
 # Keep-alive: update existing `sudo` time stamp until finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Install Brew
+# Homebrew
 if hash brew 2>/dev/null; then
   echo "Homebrew is already installed!"
 else
