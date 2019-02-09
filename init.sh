de778fcfaf0b9c3bcc904d42502467ae17d85809
Dimitri Wahyudiputra <gitmitri@gmail.com>
2019-02-09 12:36:37 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -8,53 +8,43 @@ sudo chown -R $(whoami) /usr/local/bin
 
 echo
 
-# wget
+cd ~
+
 brew install wget
-
-# DNS
 brew cask install dnscrypt
-
-# Core Apps
 brew cask install iterm2
 brew cask install java
 brew cask install vlc
 brew cask install gimp
-
-# Browsers
 brew cask install google-chrome
 brew cask install firefox
 
-# Communication
-# brew cask install slack
-
-# Dev Tools
 brew install git
-# brew cask install visual-studio-code
-# brew cask install insomnia
+brew install vim
+brew install tmux
+git clone https://github.com/gpakosz/.tmux.git
+ln -s -f .tmux/.tmux.conf
+cp .tmux/.tmux.conf.local .
+
 brew cask install postman
 brew cask install macdown
-brew install heroku-toolbelt
-brew cask install expo-xde
-heroku update
-brew install docker
-brew cask install postgres
 
-# Node.js
+brew install heroku-toolbelt
+heroku update
+
+brew install docker
 brew install node
 brew install postgres
 brew install redis
 brew install mongodb
 brew install yarn
-
-# PHP
 brew install php
 
-# Android Development
 brew cask install android-sdk
 brew cask install android-studio
 brew cask install vysor
+brew cask install expo-xde
 
-# NPM Global Packages
 npm install -g sequelize-cli
 npm install -g nodemon
 npm install -g pm2
@@ -68,6 +58,10 @@ npm install -g aws-sdk
 npm install -g @vue/cli
 npm install -g express-generator
 npm install -g exp
+
+# brew cask install slack
+# brew cask install visual-studio-code
+# brew cask install insomnia
 
 # zsh and oh-my-zsh
 brew install zsh zsh-completions
