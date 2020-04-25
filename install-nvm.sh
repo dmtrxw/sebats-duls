9cd27a5acaed91923ba76c7fb9f5fb679cc05b9f
Dimitri Wahyudiputra <dimitri@Dimitris-MacBook-Pro.local>
2020-04-25 23:44:41 +0700

    Separate zsh and nvm script, updated permission

--- install-nvm.sh
+++ install-nvm.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
-
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-chsh -s $(which zsh)
-
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
 source ~/.zshrc
---
+++ install-zsh.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)" 
+chsh -s $(which zsh)
