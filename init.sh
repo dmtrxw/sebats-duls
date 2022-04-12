817d6feca2989d5cb31615587559271ea105c456
Dimitri Wahyudiputra <gitmitri@gmail.com>
2022-04-13 00:28:48 +0700

    update init.sh
    add vim plug installation

--- init.sh
+++ init.sh
@@ -43,6 +43,10 @@ curl -sSL https://get.rvm.io | bash -s stable
 # Install NVM
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 
+# Install vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 nvm install --lts
