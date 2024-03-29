75340171fd20176100dffd03588b4707a400a2e2
Dimitri <gitmitri@gmail.com>
2024-03-29 17:51:17 +0700

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,57 +5,48 @@ defaults write -g ApplePressAndHoldEnabled -bool false
 
 # Install CLI Tools
 brew install \
-wget \
-git \
-coreutils \
-vim \
-neovim \
-tmux \
-fzf \
-pygments \
 ack \
-watchman \
-efm-langserver \
-ripgrep
+coreutils \
+fzf \
+git \
+neovim \
+ripgrep \
+tmux \
+tree \
+vim \
+wget
 
 # Install GUIs
 brew install --cask \
-iterm2 \
-vlc \
-google-chrome \
+android-file-transfer \
+discord \
 firefox \
-spotify \
-the-unarchiver \
-slack \
+google-chrome \
+iterm2 \
+obs \
+postico \
 postman \
 qbittorrent \
-android-studio \
+slack \
+spotify \
+the-unarchiver \
 visual-studio-code \
-sublime-text \
-android-file-transfer \
-discord
+vlc
 
 # Setup tmux
 cd
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
 
-# Install RVM
-curl -sSL https://get.rvm.io | bash -s stable
-
 # Install NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-
-# Install vim-plug
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 nvm install --lts
 nvm use --lts
 
-# Clone public dotfiles
+# Clone personal public dotfiles
 cd
 git clone https://github.com/dmtrxw/public-dotfiles.git
 rm -rf public-dotfiles/.git/
@@ -63,15 +54,14 @@ cp -r public-dotfiles/.* .
 rm -rf public-dotfiles
 
 npm install -g \
-typescript \
-typescript-language-server \
-emmet-ls \
+json-server \
+live-server \
+neovim \
 nodemon \
 pm2 \
-neovim \
-live-server \
-json-server \
-prettier
+prettier \
+typescript-language-server \
+typescript
 
 # Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
